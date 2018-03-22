071dd03f9989ae5b0957e9f83509bd50d98f5c04
Benjamin Bowes <bbowes999@gmail.com>
2018-03-22 12:42:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var katzDeliLine = [];
+
+function takeANumber(katzDeliLine,name){
+   katzDeliLine.push(name)
+  
+return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
+
+}
+function nowServing(katzDeliLine,name){
+  katzDeliLine.shift(name)
+  return 
+  
+  
+}
